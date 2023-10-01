2c2563f0d94951ebb9f39ae2e981eedf19f2bf63
malexj <alexmaximovj@gmail.com>
2023-10-01 23:57:58 +0300

    Add metrics

---
+++ src/main/java/com/kafkaconsumer/configuration/TimedConfiguration.java
@@ -0,0 +1,8 @@
+package com.kafkaconsumer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimedConfiguration {
+
+}
--- src/main/java/com/kafkaconsumer/services/KafkaServiceImpl.java
+++ src/main/java/com/kafkaconsumer/services/KafkaServiceImpl.java
@@ -5,6 +5,8 @@ import com.kafkaconsumer.models.entity.ModelEventEntity;
 import com.kafkaconsumer.models.event.Event;
 import com.kafkaconsumer.repository.ErrorEventRepository;
 import com.kafkaconsumer.repository.ModelEventRepository;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -27,6 +29,7 @@ class KafkaServiceImpl {
     private final ErrorEventRepository errorEventRepository;
 
 
+    @Timed
     @KafkaListener(topics = "${cloud.kafka.topic}")
     public void processMessage(String message, //
                                @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions, //
@@ -34,9 +37,14 @@ class KafkaServiceImpl {
                                @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
         errorHandler(message, () -> {
             log.info("topic:{}, partition:{}, offset: {}, message: {}", topics, partitions, offsets, message);
-            ModelEventEntity entity = modelEventRepository.save(buildModelEventEntity(message));
+            ModelEventEntity entity = saveEntity(message);
             log.info("Entity with id - [{}] was successfully saved to the database ", entity.getId());
         });
+    }
+
+    @Timed("save-mangoDb")
+    private ModelEventEntity saveEntity(String message) {
+        return modelEventRepository.save(buildModelEventEntity(message));
     }
 
 
@@ -57,8 +65,14 @@ class KafkaServiceImpl {
             r.run();
         } catch (Exception ex) {
             String errorMessage = String.format("Can't process message - [%s], error - %s", message, ex.getMessage());
-            errorEventRepository.save(buildErrorEntity(errorMessage, message));
+            saveError(message, errorMessage);
         }
+    }
+
+    @Timed
+    @Counted
+    private void saveError(String message, String errorMessage) {
+        errorEventRepository.save(buildErrorEntity(errorMessage, message));
     }
 
 
